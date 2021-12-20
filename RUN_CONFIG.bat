2f48840a47f2aedc011acdba679913c71727139f
Ethan Heil <ethan.heil@mymail.champlain.edu>
2021-12-20 15:40:00 -0500

    Adding start to library config file but am confused so leaving it for now

---
+++ RUN_CONFIG.bat
@@ -0,0 +1,7 @@
+$libpath
+@echo ********************IMPORTANT*****************************
+@echo MAKE SURE THAT THE VULKAN SDK IS INSTALLED BEFORE RUNNING!
+set /p libpath=If the vulkan SDK is installed enter it's path:
+@echo %libpath%
+
+pause
