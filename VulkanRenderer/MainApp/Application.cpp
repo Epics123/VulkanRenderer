9ce212b2d52464956fa06f3a7a9816ee0a7c4bb3
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2022-02-09 16:35:43 -0500

    Adding working fragment shader texture sampler

--- VulkanRenderer/MainApp/Application.cpp
+++ VulkanRenderer/MainApp/Application.cpp
@@ -891,7 +891,7 @@ void Application::createDescriptorSetLayout()
 	samplerLayoutBinding.descriptorCount = 1;
 	samplerLayoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER;
 	samplerLayoutBinding.pImmutableSamplers = nullptr;
-	samplerLayoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
+	samplerLayoutBinding.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT;
 
 	std::array<VkDescriptorSetLayoutBinding, 2> bindings = { uboLayoutBinding, samplerLayoutBinding };
 	VkDescriptorSetLayoutCreateInfo layoutInfo{};
--- VulkanRenderer/MainApp/resources/vulkan/shaders/Test.vert
+++ VulkanRenderer/MainApp/resources/vulkan/shaders/Test.vert
@@ -9,12 +9,17 @@ layout(binding = 0) uniform UniformBufferObject
     mat4 mvp;
 }ubo;
 
+//layout(binding = 1) uniform sampler2D texSampler;
+
 layout(location = 0) in vec2 aPosition;
 layout(location = 1) in vec3 aColor;
+layout(location = 2) in vec2 aTexCoord;
 
 layout(location = 0) out vec3 fragColor;
+layout(location = 1) out vec2 fragTexCoord;
 
 void main() {
     gl_Position = ubo.mvp * vec4(aPosition, 0.0, 1.0);
     fragColor = aColor;
+    fragTexCoord = aTexCoord;
 }
