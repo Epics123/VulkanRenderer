38af3e3a5dc5452342b764c891d016d897378493
henry.chronowski <henry.chronowski@mymail.champlain.edu>
2022-02-09 12:23:06 -0500

    Adding transition to shader read only format to image loading

--- VulkanRenderer/MainApp/Application.cpp
+++ VulkanRenderer/MainApp/Application.cpp
@@ -1124,6 +1124,13 @@ void Application::createTextureImage()
 
 	Image::createImage(device, physicalDevice, texWidth, texHeight, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_TILING_OPTIMAL, VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT,
 						VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT, textureImage, textureImageMemory);
+
+	// Copy the staging buffer to the new image | Can transition from layout_undefined because we don't care about the contents before the copy operation
+	Image::transitionImageLayout(graphicsQueue, device, commandPool, textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL);
+	Image::copyBufferToImage(graphicsQueue, device, commandPool, stagingBuffer.buffer, textureImage, static_cast<uint32_t>(texWidth), static_cast<uint32_t>(texHeight));
+
+	// Transition image to format for shader read access
+	Image::transitionImageLayout(graphicsQueue, device, commandPool, textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL);
 }
 
 void Application::drawFrame()
