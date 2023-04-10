c4f56a4f49f79242b0c75f8b19cfaf3e64b241d3
Ethan Heil <eheil20@gmail.com>
2023-04-10 14:54:52 -0400

    Rotation will now be displayed in degrees on the UI

--- VulkanRenderer/MainApp/GameObject.cpp
+++ VulkanRenderer/MainApp/GameObject.cpp
@@ -4,9 +4,9 @@ glm::mat4 TransformComponent::getTransform()
 {
 	glm::mat4 transate = glm::translate(glm::mat4{ 1.0f }, translation);
 
-	glm::quat qPitch = glm::angleAxis(rotation.x, glm::vec3(1, 0, 0));
-	glm::quat qYaw = glm::angleAxis(rotation.z, glm::vec3(0, 0, 1));
-	glm::quat qRoll = glm::angleAxis(rotation.y, glm::vec3(0, 1, 0));
+	glm::quat qPitch = glm::angleAxis(glm::radians(rotation.x), glm::vec3(1, 0, 0));
+	glm::quat qYaw = glm::angleAxis(glm::radians(rotation.z), glm::vec3(0, 0, 1));
+	glm::quat qRoll = glm::angleAxis(glm::radians(rotation.y), glm::vec3(0, 1, 0));
 
 	glm::quat orientation = qPitch * qYaw * qRoll;
 	glm::mat4 rotate = glm::mat4_cast(orientation);
